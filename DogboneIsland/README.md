0e90cd55a961056a90beb3a9cb917273fb07fd0a
Sevki <sevki@shacx.com>
2012-04-10 16:56:46 +0100

    First Blood with the itallion stallion

---
+++ DogboneIsland/README.md
@@ -0,0 +1,2 @@
+# README #
+Hi, everybody!
---
+++ DogboneIsland/src/team18/cs/ncl/ac/uk/WordPair.java
@@ -0,0 +1,16 @@
+package team18.cs.ncl.ac.uk;
+
+public class WordPair {
+public String Word;
+public String Definition;
+	WordPair(String w, String d)
+	{
+		Word =w;
+		Definition = d;
+	}
+	public WordPair() {
+		// TODO Auto-generated constructor stub
+		Word= "";
+		Definition="";
+	}
+}
